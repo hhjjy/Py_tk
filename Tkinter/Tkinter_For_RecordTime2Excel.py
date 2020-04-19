fc3336e76d769e26bf616bf3959732b3f72d7094
hhjjy <lion24161582@gmail.com>
2020-04-19 22:02:48 +0800

    Only Print a Label

--- Tkinter/Tkinter_For_RecordTime2Excel.py
+++ Tkinter/Tkinter_For_RecordTime2Excel.py
@@ -1,2 +1,17 @@
-print("hello")
-pass
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Record2Excel")
+# height = num * 字體高度1單位
+# width = num * 字體長度1單位
+# font = ("Arial",字體大小)
+# bg = background
+
+Label1 = tk.Label(window, text='hello my name is Leo',
+                  height=2,
+                  width=20,
+                  font=('Arial', 12),
+                  bg='red')
+Label1.pack()
+window.geometry("200x200")
+window.mainloop()
